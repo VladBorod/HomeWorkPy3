cd9ad421ce25d26a5f397ad6121d2444f44b21f8
VladBorod <jazgigantsmith@gmail.com>
2023-02-06 14:42:36 +0300

    Closer num, hard mode edition 1.00.01, works correctly only with 7 elements and more

--- Ex2Obj18ClosestElementSearch.py
+++ Ex2Obj18ClosestElementSearch.py
@@ -20,38 +20,59 @@ os.system("cls")
 num_quantity = int(input('Введите размер списка: '))
 # Создание пустого списка--------------------
 array = []
-# Функция заполнение пустого списка случайными числами-------------------- 
+
+# Функция заполнение пустого списка случайными числами
+
 def array_filling(array, num_quantity):
     for i in range(1, num_quantity+1):
-        random_num = random.randint(1, 50)
+        random_num = random.randint(1, 20)
         array.append(random_num)
 res_array = array_filling(array, num_quantity)
+# Вывод списка--------------------
 print(array)
-# Ввод числа-ориентира для поиска ближайшего-------------------- 
+
+# Ввод числа-ориентира для поиска ближайшего
+ 
 closest_num_search = int(input('Введите число для поиска ближайшего по значению имеющегося в списке: '))
-# Функция сортировки списка, перевод списка во множество и сравнение числа ориентира с полученным множеством--------------------
-def array_sort(array, closest_num_search):
-    array.sort()
-    print(array)  
-    new_set = set(array)
-    min = 0
-    max = 0
-    for i in new_set:
-        if i == closest_num_search:
-            print(f'Число {closest_num_search} равно введенному {closest_num_search}.')
+
+# Сортировка списка (К СОЖАЛЕНИЮ КОРРЕКТНО РАБОТАЕТ СО СПИСКАМИ БОЛЬШЕ 7 ЭЛЕМЕНТОВ!!!)
+
+for i in range(num_quantity-1):
+    for j in range(num_quantity-i-1):
+        if array[j] > array[j+1]:
+            array[j], array[j+1] = array[j+1], array[j]            
+# Создание из спписка множества--------------------
+new_set = set(array)
+# Создание из множества нового списка для возможности обращения к индексам!--------------------
+array = list(new_set) 
+# Функция поиска ближадйшего числа
+def array_search(array, closest_num_search):
+    biggest_num = array[-1]
+    smallest_num = array[0]
+    # Условие превышения сравниваемого элемента элементов в списке
+    if closest_num_search < smallest_num:
+        print(f'Ближайшее число для {closest_num_search} это число {smallest_num}.')
+    elif biggest_num < closest_num_search:
+        print(f'Ближайшее число для {closest_num_search} это число {biggest_num}.')
+    else:   
+        min = 0
+        max = 0
+        for i in array:
+            if i == closest_num_search:
+                print(f'Число {closest_num_search} равно введенному {closest_num_search}.')
             # break
-        else:
-            if i < closest_num_search:
-                min = i
-            elif i > closest_num_search:
-                max = i
-                if closest_num_search - min > max - closest_num_search:
-                    print(f'Ближайшее число для {closest_num_search} это число {max}.')
-                    break
-                elif closest_num_search - min < max - closest_num_search:
-                    print(f'Ближайшее число для {closest_num_search} это число {min}.')
-                    break
-                elif closest_num_search - min == max - closest_num_search:
-                    print(f'Числа {min} и {max} одинаково близки {closest_num_search}.')
-                    break    
-sort = array_sort(array, closest_num_search)
+            else:
+                if i < closest_num_search:
+                    min = i
+                elif i > closest_num_search:
+                    max = i
+                    if closest_num_search - min > max - closest_num_search:
+                        print(f'Ближайшее число для {closest_num_search} это число {max}.')
+                        break
+                    elif closest_num_search - min < max - closest_num_search:
+                        print(f'Ближайшее число для {closest_num_search} это число {min}.')
+                        break
+                    elif closest_num_search - min == max - closest_num_search:
+                        print(f'Числа {min} и {max} одинаково близки {closest_num_search}.')
+                        break
+search = array_search(array, closest_num_search)
